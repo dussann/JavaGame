33f07139aaf0acebef956bf0fee65b478b9b2132
Dusan <dussann1989@outlook.com>
2018-07-29 20:06:31 +0200

    add track and obstacle

--- MyGame/src/Game.java
+++ MyGame/src/Game.java
@@ -1,13 +1,48 @@
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.shape.Box;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 public class Game extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		System.out.println("Idemo...");
-		System.out.println("eee");
+		 //Drawing a Box
+	      Box traka = new Box(5, 1, 15);
+	      Box prepreka = new Box(2,2,2);
+
+
+
+	      //Creating a Group object
+	      Group root = new Group(traka);
+	      root.getChildren().add(prepreka);
+
+	      PerspectiveCamera camera = new PerspectiveCamera(true);
+
+			camera.setRotationAxis(Rotate.X_AXIS);
+			camera.setRotate(-30);
+
+			camera.setTranslateY(-20);
+			camera.setTranslateZ(-30);
+			camera.setTranslateX(10);
+
+			camera.setFieldOfView(45);
+
+	      //Creating a scene object
+	      Scene scene = new Scene(root, 600, 300);
+	      scene.setCamera(camera);
+	      //Setting title to the Stage
+	      primaryStage.setTitle("Drawing a Box");
+
+	      //Adding scene to the stage
+	      primaryStage.setScene(scene);
+
+	      //Displaying the contents of the stage
+	      primaryStage.show();
 	}
 
 	public static void main(String[] args) {
